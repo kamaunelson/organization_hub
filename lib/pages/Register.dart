3904b483dc3c77fa4e39cd450fc3238ef4f43ffe
Nelson Kamau <nelson.kaguamere@strathmore.edu>
2024-02-24 20:12:45 +0300

    added key details to the Register.dart page

--- lib/pages/Register.dart
+++ lib/pages/Register.dart
@@ -5,6 +5,65 @@ class Register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Register')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality for submitting the registration details
+              },
+              child: const Text('Submit'),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already have an account? "),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context); // Navigate back to the login page
+                  },
+                  child: const Text(
+                    'Login here', // Text for the login link
+                    style: TextStyle(
+                      color: Colors.blue, // Customize the color of the login link
+                      decoration: TextDecoration.none, // Add underline decoration to the text
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
